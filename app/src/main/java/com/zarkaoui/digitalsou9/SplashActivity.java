fab7603df1f32950a2f29325a2880262b709c7f3
Zarkaoui <zarkaoui99@gmail.com>
2022-06-14 21:52:56 +0100

    Project setup

---
+++ app/src/main/java/com/zarkaoui/digitalsou9/SplashActivity.java
@@ -0,0 +1,45 @@
+package com.zarkaoui.digitalsou9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        LottieAnimationView lottieAnimationView = findViewById(R.id.lottieAnimationView);
+        lottieAnimationView.animate().translationX(-1000).setDuration(1000).setStartDelay(2500);
+
+        TextView splashText = findViewById(R.id.splashText);
+        splashText.animate().translationX(1000).setDuration(1000).setStartDelay(2500);
+
+        Thread thread = new Thread() {
+
+            public void run(){
+                try{
+                    Thread.sleep(4000);
+                } catch (Exception e){
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+
+        thread.start();
+    }
+}
