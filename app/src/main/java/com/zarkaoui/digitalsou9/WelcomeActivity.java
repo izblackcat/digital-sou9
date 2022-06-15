e22fb819302a70ee1604b818c03cb3eaadb47b78
Zarkaoui <zarkaoui99@gmail.com>
2022-06-15 20:11:42 +0100

    Add customized logo and icon

--- app/src/main/java/com/zarkaoui/digitalsou9/WelcomeActivity.java
+++ app/src/main/java/com/zarkaoui/digitalsou9/WelcomeActivity.java
@@ -5,12 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("real");
+
+        myRef.setValue("For real");
     }
 
     public void onButtonSignUpClicked(View view){
