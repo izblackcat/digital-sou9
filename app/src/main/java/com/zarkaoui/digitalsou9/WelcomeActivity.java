e8c7af11dcd3ae3a1571a2f63914b97ba4567cd1
Zarkaoui <zarkaoui99@gmail.com>
2022-06-15 03:39:44 +0100

    Finish animation screen and welcome activity

---
+++ app/src/main/java/com/zarkaoui/digitalsou9/WelcomeActivity.java
@@ -0,0 +1,21 @@
+package com.zarkaoui.digitalsou9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+
+    public void onButtonSignUpClicked(View view){
+
+    }
+
+    public void onButtonSignInClicked(View view){}
+}
